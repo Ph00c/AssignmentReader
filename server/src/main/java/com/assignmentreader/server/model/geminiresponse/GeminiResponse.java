3003c5a7d5f9f6bea13cc05fee83cb32680c7e17
Phuc Tran <phucmhtran@gmail.com>
2026-04-11 23:25:49 -0500

    added canidate

--- server/src/main/java/com/assignmentreader/server/model/geminiresponse/GeminiResponse.java
+++ server/src/main/java/com/assignmentreader/server/model/geminiresponse/GeminiResponse.java
@@ -1,4 +1,11 @@
 package com.assignmentreader.server.model.geminiresponse;
 
+import java.util.List;
+
 public class GeminiResponse {
+    private List<Canidates> canidateslist;
+
+    public GeminiResponse(Content content) {
+        this.content = content;
+    }
 }
